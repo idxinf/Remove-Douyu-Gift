762841df15a0c2782bf3903f1c20005fa184d994
idxinf <imfine233@outlook.com>
2026-01-22 20:36:34 +0800

    build: 构建主脚本

---
+++ index.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name            Remove Douyu Gift - 隐藏斗鱼礼物栏
+// @namespace       https://github.com/idxinf/Remove-Douyu-Gift
+// @version         1.0.0
+// @description     隐藏斗鱼礼物栏，保留和美化工具栏
+// @author          idxinf
+// @match			*://*.douyu.com/0*
+// @match			*://*.douyu.com/1*
+// @match			*://*.douyu.com/2*
+// @match			*://*.douyu.com/3*
+// @match			*://*.douyu.com/4*
+// @match			*://*.douyu.com/5*
+// @match			*://*.douyu.com/6*
+// @match			*://*.douyu.com/7*
+// @match			*://*.douyu.com/8*
+// @match			*://*.douyu.com/9*
+// @icon            https://www.douyu.com/favicon.ico
+// @grant           GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`css`);
+})();
